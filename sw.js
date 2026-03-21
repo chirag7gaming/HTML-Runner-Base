13fc87d5502d16f2be2f7ba563d1439b6ec013bb
chirag7gaming <chikushylendra@gmail.com>
2026-03-21 21:55:10 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', function(e) {
-    self.skipWaiting();
-});
-self.addEventListener('activate', function(e) {
-    clients.claim();
-});
-self.addEventListener('fetch', function(e) {
-    e.respondWith(fetch(e.request));
-});
